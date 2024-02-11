4350a037f631ad1389a1375c5d93314769a7dae2
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2024-02-10 19:46:10 -0800

    Add storage for api

--- app/api/sessions/route.ts
+++ app/api/sessions/route.ts
@@ -1,19 +1,26 @@
 import { NextResponse, NextRequest } from 'next/server';
 
+declare type SessionRow = {
+    sessionId: string | null;
+    rows: string | null;
+}
+
+const storage: Array<SessionRow> = new Array<SessionRow>;
+
 function storeInDb(sessionId: string | null, rows: string | null) {
     // TODO: store in db
     console.log(`sessionId: ${sessionId}`);
     console.log(`rows: ${rows}`);
+    storage.push({sessionId, rows});
 }
 
 export async function GET(req: NextRequest) {
     const sessionId = req.nextUrl.searchParams.get('sessionId');
     const rows = req.nextUrl.searchParams.get('rows');
 
+    console.log(rows);
+
     storeInDb(sessionId, rows)
 
-    return NextResponse.json({
-        sessionId,
-        rows
-    });
+    return NextResponse.json(storage);
 }
