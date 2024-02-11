4867a34002986fdf9b20854acbb8f5ce6f8d5459
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2024-02-10 20:42:48 -0800

    Add lastRow parameter

--- app/api/sessions/route.ts
+++ app/api/sessions/route.ts
@@ -3,24 +3,40 @@ import { NextResponse, NextRequest } from 'next/server';
 declare type SessionRow = {
     sessionId: string | null;
     rows: string | null;
+    lastRow: string | null;
+    updated: number
 }
 
-const storage: Array<SessionRow> = new Array<SessionRow>;
+let storage: Array<SessionRow> = new Array<SessionRow>;
 
-function storeInDb(sessionId: string | null, rows: string | null) {
+function storeInDb(sessionId: string | null, rows: string | null, lastRow: string | null) {
     // TODO: store in db
     console.log(`sessionId: ${sessionId}`);
     console.log(`rows: ${rows}`);
-    storage.push({sessionId, rows});
+
+    const r = storage.find((row) => row.sessionId === sessionId);
+
+    if (!r) {
+        storage.push({sessionId, rows, lastRow, updated: 0});
+    } else {
+        let updated = 0;
+        if (r.rows != rows) {
+            updated += 1;
+        }
+        if (r.lastRow != lastRow) {
+            updated += 2;
+        }
+
+        storage = storage.map((row) => row.sessionId === sessionId ? {sessionId, row, lastRow, updated} : row) as Array<SessionRow>;
+    }
 }
 
 export async function GET(req: NextRequest) {
     const sessionId = req.nextUrl.searchParams.get('sessionId');
     const rows = req.nextUrl.searchParams.get('rows');
+    const lastRow = req.nextUrl.searchParams.get('v');
 
-    console.log(rows);
-
-    storeInDb(sessionId, rows)
+    storeInDb(sessionId, rows, lastRow);
 
     return NextResponse.json(storage);
 }
